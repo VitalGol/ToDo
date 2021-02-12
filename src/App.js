61071fb88c4d4bb5479ea9707f3361f09b8b1017
Vitaly <vitalgolob@gmail.com>
2021-02-12 16:41:58 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
+import { FormControl, Button, Input, InputLabel } from "@material-ui/core";
+import Todo from "./Todo";
 
 function App() {
+  const [todos, setTodos] = useState(["one", "two", "three"]);
+  const [input, setInput] = useState("");
+  const handleChange = (e) => {
+    setInput(e.target.value);
+  };
+  const handleClick = (e) => {
+    e.preventDefault();
+    setTodos([...todos, input]);
+    setInput("");
+  };
+  let newTodos = todos.map((todo) => <p>{todo}</p>);
+  useEffect(() => {
+    console.log("--> ", todos);
+  }, [todos]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <FormControl>
+        <InputLabel>Write a ToDO</InputLabel>
+        <Input type="text" value={input} onChange={handleChange} />
+      </FormControl>
+
+      <Button
+        disabled={!input}
+        variant="contained"
+        color="primary"
+        type="submit"
+        onClick={handleClick}
+      >
+        Add Todo
+      </Button>
+      <Todo todos={newTodos} />
     </div>
   );
 }
-
 export default App;
